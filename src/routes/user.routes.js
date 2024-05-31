57e70225d05ebd5e20f09c364028393db1718b47
Vishal4869 <vaumtol@gmail.com>
2024-06-01 02:01:04 +0530

    Demo template completed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { 
+    registerUser,
     loginUser, 
     logoutUser, 
     refreshAccessToken, 
@@ -9,7 +10,15 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
-
+router.route("/register").post(
+    // upload.fields([
+    //     {
+    //         name: "avatar",
+    //         maxCount: 1
+    //     }, 
+    // ]),
+    registerUser
+    )
 
 router.route("/login").post(loginUser)
 
